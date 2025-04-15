5de8ee3217d81596b3a71e508efbcd92c0b8d70f
matjoseph24 <mathew.a.joseph@accenture.com>
2025-04-15 14:56:53 +0100

    DL-15950: Modified order of precheck script commands

--- precheck.sh
+++ precheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbt clean scalafmt test:scalafmt coverage it/Test/scalafmt it/test test coverageReport
+sbt clean scalafmt test:scalafmt it/Test/scalafmt coverage test it/test coverageReport
